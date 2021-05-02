8391e2b4ec461383d3bc253ab856ca584639cf38
CatoMaior <CatoMaior@users.noreply.github.com>
2021-05-02 10:22:15 +0000

    Publish 'docs/html' to 'gh-pages' in 'CatoMaior/myLibrary'

---
+++ search/all_c.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['printmatrix_25',['printMatrix',['../arrays_8h.html#aa607e7ac224a243add6993c972d07fa9',1,'arrays.h']]],
+  ['ptrcmp_26',['ptrCmp',['../utility_8h.html#a3a26e3c3ede0beca95ea69c8372bbda5',1,'utility.h']]]
+];
---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mylibrary_20homepage_81',['myLibrary homepage',['../index.html',1,'']]]
+];
