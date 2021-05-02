6346f8c1a0a41745854909672908be07fa11539d
CatoMaior <pietrosenzacognome@gmail.com>
2021-05-02 11:09:25 +0200

    New theme

---
+++ docs/html/constants_8h.js
@@ -0,0 +1,12 @@
+var constants_8h =
+[
+    [ "EQUAL", "constants_8h.html#a214c717b2e51e1993a749ac99df7de58", null ],
+    [ "FALSE", "constants_8h.html#aa93f0eb578d23995850d61f7d61c55c1", null ],
+    [ "GREATER", "constants_8h.html#ae27a03218878a9af9d7d946e186f8598", null ],
+    [ "KEY_NOT_FOUND", "constants_8h.html#a12f8ec8b6434aa0f09f83687458af46a", null ],
+    [ "SMALLER", "constants_8h.html#aac275767d3931bb4649e508153721044", null ],
+    [ "SUCCESS", "constants_8h.html#aa90cac659d18e8ef6294c7ae337f6b58", null ],
+    [ "TRUE", "constants_8h.html#aa8cecfc5c5c054d2875c03e77b7be15d", null ],
+    [ "UNKNOWN_SPEC", "constants_8h.html#a2e7b54758861af710f00866d9b71f717", null ],
+    [ "UNSUPPORTED_ARCHITECTURE", "constants_8h.html#aaf0e0fcdf1f618e3278d5edced9ad25a", null ]
+];
---
+++ docs/html/strings_8h.js
@@ -0,0 +1,8 @@
+var strings_8h =
+[
+    [ "changeLastCharacter", "strings_8h.html#aa2af9f4f6ecbe3950741db2e19ec604e", null ],
+    [ "copyOf", "strings_8h.html#a899c069743717f0b01fd9dffdaa3404b", null ],
+    [ "endsWith", "strings_8h.html#ae2d2518b7d79536b53f6b590b5de2efc", null ],
+    [ "getLength", "strings_8h.html#a8e9948ec41d425e3831795c9a24a55a9", null ],
+    [ "getString", "strings_8h.html#a07fe93f1edbc9d815058eca59e9b2636", null ]
+];
