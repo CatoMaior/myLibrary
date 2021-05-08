c105fef435fa9f9c4ebc27b6bef48c01757dcba6
CatoMaior <pietrosenzacognome@gmail.com>
2021-05-08 10:50:53 +0200

    Added missing spaces

--- macros.h
+++ macros.h
@@ -21,7 +21,7 @@
  * @retval EQUAL First element is equal to the second
  * @retval SMALLER First element is smaller than the second
  */
-#define cmp(a, b) _Generic((a, b), char: charCmp, int:intCmp, float: floatCmp, double: doubleCmp, void *:ptrCmp)(&a, &b) 
+#define cmp(a, b) _Generic((a, b), char: charCmp, int: intCmp, float: floatCmp, double: doubleCmp, void *: ptrCmp)(&a, &b) 
 
 /**
  * @brief BubbleSort for arrays
