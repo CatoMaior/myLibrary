459b216678a171888de5e9685f34284ec2f665ea
CatoMaior <pietrosenzacognome@gmail.com>
2021-05-09 16:54:05 +0200

    Added many functions

--- arrayList.h
+++ arrayList.h
@@ -9,5 +9,124 @@
 
 #include "types.h"
 
+/**
+ * @brief Append an element to an ::ArrayList
+ * @param arr The ::ArrayList you want to append an item to
+ * @param element The element you want to append to `arr`
+ */
+#define appendToAL(arr, element) _Generic(element, char: appendCharToAL)(arr, element)
+
+/**
+ * @brief Create an ::ArrayList from a static array
+ * @param arr The array you want to create an ::ArrayList from
+ * @param size The size of `arr`
+ * @return An ::ArrayList containing all the elements of `arr`void
+ */
+#define newALFromArray(arr, size) _Generic(arr, char*: newALFromCharArray)(arr, size)
+
+// TYPE INDIPENDENT FUNCTIONS
+/**
+ * @brief Allocate a new ::ArrayList
+ * @return  An empty ::ArrayList
+ */
+ArrayList newAL();
+
+/**
+ * @brief Get a copy of an ::ArrayList
+ * @param arr The ::ArrayList you want to copy
+ * @return A copy of `arr`
+ */
+ArrayList newALFromAL(const ArrayList arr);
+
+/**
+ * @brief Merge two ::ArrayList
+ * @param arr1 The first ::ArrayList to be merged, where the merged ::ArrayList is saved
+ * @param arr2 The second ::ArrayList to be merged
+ */
+void mergeAL(ArrayList arr1, const ArrayList arr2);
+
+/**
+ * @brief Slice an ::ArrayList
+ * @param arr The ::ArrayList you want to slice, where the sliced ::ArrayList is saved
+ * @param begin The index of the beginning of the slice
+ * @param end The index of the end of the slice
+ */
+void sliceAL(ArrayList arr, unsigned int begin, unsigned int end);
+
+/**
+ * @brief Print an ::ArrayList content
+ * @param spec The type and format specifier you want to use to print the single element of the ::ArrayList
+ * @param arr The ::ArrayList you want to print
+ */
+void printAL(const spec_t spec, const ArrayList arr);
+
+/**
+ * @brief Remove an element from an ::ArrayList
+ * @param arr The ::ArrayList you want to delete an element from
+ * @param index The index of the element you want to delete
+ */
+void removeFromAL(ArrayList arr, unsigned int index);
+
+/**
+ * @brief Get an element from an ::ArrayList
+ * @param arr The ::ArrayList you want to get the item from
+ * @param index The index of the element you want to get
+ * @param dest The address of the variable you want to store the result in
+ */
+void getFromAL(const ArrayList arr, unsigned int index, void *dest);
+
+/**
+ * @brief Delete an ::ArrayList
+ * @param arr The ::ArrayList you want to delete
+ */
+void deleteAL(ArrayList arr);
+
+/**
+ * @brief Compare two ::ArrayList
+ * @param arr1 The first ::ArrayList you want to compare
+ * @param arr2 The second ::ArrayList you want to compare
+ * @return The result of the comparison
+ * @retval TRUE `arr1` and `arr2` have equal type, equal length and equal contents
+ * @retval FALSE `arr1` and `arr2` do not have equal type, equal length or equal contents
+ */
+byte compareAL(ArrayList arr1, ArrayList arr2);
+
+/**
+ * @brief Reverse an ::ArrayList
+ * @param arr The array you want to reverse
+ */
+void reverseAL(ArrayList arr);
+
+// TYPE DIPENDENT FUNCTIONS
+/**
+ * @brief Create an ::ArrayList from an array of chars
+ * @param arr The array you want to create the ::ArrayList from
+ * @param size The size of `arr`
+ * @return An ::ArrayList of type char containing the elements in `arr` in the same order
+ */
+ArrayList newALFromCharArray(const char arr[], unsigned int size);
+
+/**
+ * @brief Insert a char at the end of an ::ArrayList 
+ * @param arr The ::ArrayList you want to append a char to
+ * @param element The char you want to append to `arr`
+ */
+void appendCharToAL(ArrayList arr, char element);
+
+/**
+ * @brief Insert a char at a specified position of an ::ArrayList
+ * @param arr The ::ArrayList you want to insert the char into
+ * @param element The char you want to insert into `arr`
+ * @param index The position you want to insert `element` at
+ */
+void insertCharToAL(ArrayList arr, char element, unsigned int index);
+
+/**
+ * @brief Set value of an element of an ::ArrayList
+ * @param arr The ::ArrayList you want to edit
+ * @param element The element you want to set 
+ * @param index The index of the element you want to change
+ */
+void setALChar(ArrayList arr, char element, unsigned int index);
 
 #endif
--- src/arrayList.c
+++ src/arrayList.c
@@ -1,3 +1,168 @@
+#include "../constants.h"
+#include "../strings.h"
 #include "../types.h"
 #include "../utility.h"
+#include "errors.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+ArrayList newAL() {
+    ArrayList newArray = saferMalloc(sizeof(ArrayList));
+    newArray->size = 0;
+    return newArray;
+}
+
+ArrayList newALFromAL(const ArrayList arr) {
+    checkCondition(!arr, NULL_AL_GIVEN);
+    ArrayList copy = newAL();
+    copy->size = arr->size;
+    unsigned int bytesToBeCopied;
+    if (strcmp(arr->type, "%c") == 0) {
+        copy->type = "%c";
+        bytesToBeCopied = arr->size * sizeof(char);
+    }
+    copy->body = saferRealloc(copy->body, bytesToBeCopied);
+    memcpy(copy->body, arr->body, bytesToBeCopied);
+    return copy;
+}
+
+void mergeAL(ArrayList arr1, const ArrayList arr2) {
+    checkCondition(!arr1 || !arr2, NULL_AL_GIVEN);
+    checkCondition(!arr1->type || !arr2->type, NULL_AL_TYPE);
+    checkCondition(strcmp(arr1->type, arr2->type) != 0, DIFFERENT_AL_TYPES);
+    unsigned int bytesToBeCopied;
+    byte typeSize;
+    if (strcmp(arr1->type, "%c") == 0) {
+        typeSize = sizeof(char);
+        bytesToBeCopied = arr2->size * sizeof(char);
+    }
+    arr1->body = saferRealloc(arr1->body, (arr1->size + arr2->size) * typeSize);
+    memcpy(arr1->body + arr1->size * typeSize, arr2->body, bytesToBeCopied);
+    arr1->size = arr1->size + arr2->size;
+}
+
+void sliceAL(ArrayList arr, unsigned int begin, unsigned int end) {
+    checkCondition(!arr, NULL_AL_GIVEN);
+    checkCondition(!arr->type, NULL_AL_TYPE);
+    checkCondition(begin < 0 || end > arr->size, OUT_OF_AL_BOUNDS);
+    checkCondition(begin > end, WRONG_INDEX_ORDER);
+    byte typeSize;
+    void *newBody;
+    if (strcmp(arr->type, "%c") == 0)
+        typeSize = sizeof(char);
+    arr->size = abs(end - begin);
+    newBody = saferMalloc(arr->size * typeSize);
+    memcpy(newBody, arr->body + begin * typeSize, arr->size * typeSize);
+    free(arr->body);
+    arr->body = newBody;
+}
+
+void printAL(const spec_t spec, const ArrayList arr) {
+    checkCondition(!arr, NULL_AL_GIVEN);
+    checkCondition(!spec, NULL_POINTER_GIVEN);
+    checkCondition(!arr->type, NULL_AL_TYPE);
+    if (arr->size == 0) {
+        printf("Empty\n");
+        return;
+    }
+    if (strcmp(arr->type, "%c") == 0)
+        for (unsigned int i = 0; i < arr->size; i++)
+            printf(spec, *((char *)arr->body + i * sizeof(char)));
+}
+
+void removeFromAL(ArrayList arr, unsigned int index) {
+    checkCondition(!arr, NULL_AL_GIVEN);
+    checkCondition(index >= arr->size, OUT_OF_AL_BOUNDS);
+    if (arr->size == 1) {
+        free(arr->body);
+        arr->size--;
+        return;
+    }
+    byte typeSize;
+    if (strcmp(arr->type, "%c") == 0)
+        typeSize = sizeof(char);
+    memcpy(arr->body + index * typeSize, arr->body + (index + 1) * typeSize, (arr->size - index) * typeSize);
+    arr->body = saferRealloc(arr->body, --arr->size * typeSize);
+}
+
+void getFromAL(const ArrayList arr, unsigned int index, void *dest) {
+    checkCondition(!arr, NULL_AL_GIVEN);
+    checkCondition(!dest, NULL_DESTINATION_GIVEN);
+    checkCondition(index >= arr->size, OUT_OF_AL_BOUNDS);
+    if (strcmp(arr->type, "%c") == 0)
+        *(char *)dest = *((char *)arr->body + index * sizeof(char));
+}
+
+byte compareAL(ArrayList arr1, ArrayList arr2) {
+    checkCondition(!arr1 || !arr2, NULL_AL_GIVEN);
+    if (strcmp(arr1->type, arr2->type) != 0 || arr1->size != arr2->size)
+        return FALSE;
+    byte typeSize;
+    if (strcmp(arr1->type, "%c") == 0)
+        typeSize = sizeof(char);
+    if (memcmp(arr1->body, arr2->body, arr1->size * typeSize) == 0)
+        return TRUE;
+    return FALSE;
+}
+
+void appendCharToAL(ArrayList arr, char element) {
+    checkCondition(!arr, NULL_AL_GIVEN);
+    if (!arr->type)
+        arr->type = "%c";
+    if (arr->size == 0)
+        arr->body = saferMalloc(sizeof(char));
+    else
+        arr->body = saferRealloc(arr->body, sizeof(char));
+    *((char *)arr->body + arr->size * sizeof(char)) = element;
+    arr->size++;
+}
+
+ArrayList newALFromCharArray(const char arr[], unsigned int size) {
+    checkCondition(!arr, NULL_AL_GIVEN);
+    ArrayList newArray = saferMalloc(sizeof(ArrayList));
+    newArray->type = "%c";
+    newArray->size = 0;
+    for (unsigned int i = 0; i < size; i++)
+        appendCharToAL(newArray, arr[i]);
+    return newArray;
+}
+
+void deleteAL(ArrayList arr) {
+    checkCondition(!arr, NULL_AL_GIVEN);
+    free(arr->body);
+    free(arr);
+}
+
+void insertCharToAL(ArrayList arr, char element, unsigned int index) {
+    checkCondition(!arr, NULL_AL_GIVEN);
+    checkCondition(!arr->type, NULL_AL_TYPE);
+    checkCondition(index >= arr->type, OUT_OF_AL_BOUNDS);
+    void *newBody = saferMalloc(arr->size + 1 * sizeof(char));
+    memcpy(newBody, arr->body, index * sizeof(char));
+    *((char *)newBody + index * sizeof(char)) = element;
+    memcpy(newBody + (index + 1) * sizeof(char), arr->body + index * sizeof(char), (arr->size - index) * sizeof(char));
+    arr->size++;
+    free(arr->body);
+    arr->body = newBody;
+}
+
+void setALChar(ArrayList arr, char element, unsigned int index) {
+    checkCondition(!arr, NULL_AL_GIVEN);
+    checkCondition(index >= arr->size, OUT_OF_AL_BOUNDS);
+    *((char *)arr->body + index * sizeof(char)) = element;
+}
+
+void reverseAL(ArrayList arr) {
+    checkCondition(!arr, NULL_AL_GIVEN);
+    void *newBody;
+    if (strcmp(arr->type, "%c") == 0) {
+        newBody = saferMalloc(arr->size * sizeof(char));
+        char temp;
+        for (unsigned int i = 0; i < arr->size; i++)
+            getFromAL(arr, i, newBody + (arr->size - i - 1) * sizeof(char));
+    }
+    free(arr->body);
+    arr->body = newBody;
+}
