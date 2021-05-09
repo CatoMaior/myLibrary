fab28c31b453a08ef3854d8c79ef8124e9cd6e89
CatoMaior <pietrosenzacognome@gmail.com>
2021-05-09 16:50:41 +0200

    Created file errors.h

---
+++ src/errors.h
@@ -0,0 +1,19 @@
+#ifndef _SEEN_ERRORS
+#define _SEEN_ERRORS
+
+#define NULL_AL_TYPE "Error: null ArrayList type"
+
+#define NULL_DESTINATION_GIVEN "Error: null destination pointer given"
+
+#define OUT_OF_AL_BOUNDS "Error: trying to access ArrayList out of its bounds"
+
+
+#define NULL_POINTER_GIVEN "Error: null pointer given"
+
+#define NULL_AL_GIVEN "Error: null ArrayList given"
+
+#define WRONG_INDEX_ORDER "Error: begin index greater than end index"
+
+#define DIFFERENT_AL_TYPES "Error: different ArrayList types"
+
+#endif
