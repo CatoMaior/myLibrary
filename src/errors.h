0accca9f95a2053c57c9904e38a3941d47d39d7e
CatoMaior <pietrosenzacognome@gmail.com>
2021-05-10 12:13:10 +0200

    Added some error strings

--- src/errors.h
+++ src/errors.h
@@ -7,6 +7,7 @@
 
 #define OUT_OF_AL_BOUNDS "Error: trying to access ArrayList out of its bounds"
 
+#define UNSUPPORTED_SPECIFIER "Error: given specifier was not recognized or is not supported"
 
 #define NULL_POINTER_GIVEN "Error: null pointer given"
 
@@ -15,5 +16,11 @@
 #define WRONG_INDEX_ORDER "Error: begin index greater than end index"
 
 #define DIFFERENT_AL_TYPES "Error: different ArrayList types"
+
+#define UNSUPPORTED_ARCHITECTURE "Error: pointers have unsupported size"
+
+#define ALLOC_ERROR "Error: could not allocate memory"
+
+#define REALLOC_ERROR "Error: could not reallocate memory"
 
 #endif
