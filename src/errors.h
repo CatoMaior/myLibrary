ca70d97d0c49b9bbb056afc6f50dcfc96c8ca7a8
CatoMaior <pietrosenzacognome@gmail.com>
2021-05-16 23:03:52 +0200

    Changed error strings; added some

--- src/errors.h
+++ src/errors.h
@@ -1,28 +1,34 @@
 #ifndef _SEEN_ERRORS
 #define _SEEN_ERRORS
 
-#define NULL_AL_TYPE "Error: null ArrayList type"
+#define NULL_AL_TYPE "Null ArrayList type"
 
-#define NULL_DESTINATION_GIVEN "Error: null destination pointer given"
+#define NULL_DESTINATION_GIVEN "Null destination pointer given"
 
-#define OUT_OF_AL_BOUNDS "Error: trying to access ArrayList out of its bounds"
+#define OUT_OF_AL_BOUNDS "Trying to access ArrayList out of its bounds"
 
-#define UNSUPPORTED_SPECIFIER "Error: given specifier was not recognized or is not supported"
+#define UNSUPPORTED_SPECIFIER "Given specifier was not recognized or is not supported"
 
-#define NULL_POINTER_GIVEN "Error: null pointer given"
+#define NULL_POINTER_GIVEN "Null pointer given"
 
-#define NULL_AL_GIVEN "Error: null ArrayList given"
+#define NULL_AL_GIVEN "Null ArrayList given"
 
-#define WRONG_INDEX_ORDER "Error: begin index greater than end index"
+#define WRONG_INDEX_ORDER "Begin index greater than end index"
 
-#define DIFFERENT_AL_TYPES "Error: different ArrayList types"
+#define DIFFERENT_AL_TYPES "Different ArrayList types"
 
-#define UNSUPPORTED_ARCHITECTURE "Error: pointers have unsupported size"
+#define UNSUPPORTED_ARCHITECTURE "Pointers have unsupported size"
 
-#define ALLOC_ERROR "Error: could not allocate memory"
+#define ALLOC_ERROR "Could not allocate memory"
 
-#define REALLOC_ERROR "Error: could not reallocate memory"
+#define REALLOC_ERROR "Could not reallocate memory"
 
-#define NULL_AL_BODY "Error: given ArrayList has null body"
+#define NULL_AL_BODY "Given ArrayList has null body"
+
+#define NULL_LL_GIVEN "Null LinkedList given"
+
+#define NULL_LL_TYPE "Null LinkedList type"
+
+#define NULL_LL_HEAD "Given LinkedList has null head"
 
 #endif
